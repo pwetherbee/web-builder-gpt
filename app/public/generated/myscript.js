dbd227fa77b64eee1dfa9f513897fe8634e05b1e
Patrick Wetherbee <pwetherb96@gmail.com>
2023-04-04 02:36:37 -0700

    removed generated

--- app/public/generated/myscript.js
+++
@@ -1,4 +0,0 @@
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
-    window.location.href = 'generate.html';
-});
--- app/public/generated/script.js
+++
@@ -1,8 +0,0 @@
-
-```js
-const generateButton = document.getElementById("generate-button");
-
-generateButton.addEventListener("click", () => {
-  alert("Sorry, the website generation feature is not available yet. Please check back soon!");
-});
-```
